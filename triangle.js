10cdd77ba58fe1b5f09e5608be87e2f4780fde18
indhu1105 <indhumathi.mani@trustrace.com>
2020-05-20 18:05:42 +0530

    Add js file

---
+++ triangle.js
@@ -0,0 +1,10 @@
+let height = +prompt("enter the height",0);
+let breadth = +prompt("enter the breadth",0);
+let result;
+
+function area()
+{
+   result = (height * breadth) / 2;
+   console.log(result);
+   return result;
+}
